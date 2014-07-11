ffc939edb4a2f70806e154c5da3195b472af35b4
j1wu <ji.wu@outlook.com>
2014-07-11 08:12:24 +0800

    categories added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: 'Annie Hall', description: 'N/A', small_cover_url: '/tmp/annie_hall.jpg', large_cover_url: '/tmp/sample.jpg')
-Video.create(title: 'Avengers', description: 'N/A', small_cover_url: '/tmp/avengers.jpg', large_cover_url: '/tmp/sample.jpg')
-Video.create(title: 'Dark Night', description: 'N/A', small_cover_url: '/tmp/dark_knight.jpg', large_cover_url: '/tmp/sample.jpg')
-Video.create(title: 'Godfather', description: 'N/A', small_cover_url: '/tmp/godfather.jpg', large_cover_url: '/tmp/sample.jpg')
+drama = Category.create(name: 'Drama')
+scifi = Category.create(name: 'Sci-Fi')
+
+Video.create(title: 'Annie Hall', description: 'N/A', small_cover_url: '/tmp/annie_hall.jpg', large_cover_url: '/tmp/sample.jpg', category: drama)
+Video.create(title: 'Avengers', description: 'N/A', small_cover_url: '/tmp/avengers.jpg', large_cover_url: '/tmp/sample.jpg', category: scifi)
+Video.create(title: 'Dark Night', description: 'N/A', small_cover_url: '/tmp/dark_knight.jpg', large_cover_url: '/tmp/sample.jpg', category: scifi)
+Video.create(title: 'Godfather', description: 'N/A', small_cover_url: '/tmp/godfather.jpg', large_cover_url: '/tmp/sample.jpg', category: drama)
