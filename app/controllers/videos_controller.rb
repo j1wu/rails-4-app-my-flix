6adc5765bcff04fc0c4cea299b42b09cc2be5786
j1wu <ji.wu@outlook.com>
2014-07-13 00:41:14 +0800

    implemented search in UI

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,4 +8,8 @@ def show
   @video = Video.find(params[:id])
 end
 
+def search
+  @results = Video.search_by_title(params[:search_term])
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Myflix::Application.routes.draw do
   resources :videos do
-    
+    collection do
+      post 'search', to: 'videos#search'
+    end 
   end
 
   resources :categories, only: [:show]
