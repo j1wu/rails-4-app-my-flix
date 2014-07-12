1a627a73263ea8c9134d789429e13fa2b0f6c246
j1wu <ji.wu@outlook.com>
2014-07-12 09:59:27 +0800

    used shoulda matcher to test model association and validation

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,17 +2,6 @@ require 'spec_helper'
 
 describe Category do
 
-  it 'saves category' do
-    drama = Category.new(name: 'Drama')
-    drama.save
-    Category.first.name.should == 'Drama'
-  end
-
-  it 'has many videos' do
-    drama = Category.create(name: 'Drama')
-    godfather = Video.create(title: 'God Father', description: 'n/a', category: drama)
-    anniehall = Video.create(title: 'Annie Hall', description: 'n/a', category: drama)
-    expect(drama.videos).to eq([anniehall, godfather])
-  end
-
+  it {should have_many :videos}
+  
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,26 +2,8 @@ require 'spec_helper'
 
 describe Video do
 
-  it 'saves video' do
-    video = Video.new(title: 'Fight Club', description: 'N/A')
-    video.save
-    Video.first.title.should == 'Fight Club'
-  end
-
-  it 'belongs to category' do
-    drama = Category.create(name: 'Drama')
-    godfather = Video.create(title: 'God Father', category: drama)
-    expect(godfather.category).to eq(drama)
-  end
-
-  it 'must has title' do
-    video = Video.create(title: '', description: 'n/a')
-    expect(Video.all.count).to eq(0)
-  end
-
-  it 'must has description' do 
-    video = Video.create(title: 'Fight Club', description: '')
-    expect(Video.all.count).to eq(0)
-  end
+  it {should belong_to :category}
+  it {should validate_presence_of :title}
+  it {should validate_presence_of :description}
   
 end
