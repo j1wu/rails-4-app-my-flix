a8c16d89c8b27b0a9f7416875ba8f5bc885df9a8
j1wu <ji.wu@outlook.com>
2014-07-23 15:00:26 +0800

    user can review a video

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  before_action :require_user
+
+  def create
+    video = Video.find(params[:video_id])
+    review = video.reviews.build(post_params.merge!(user: current_user))
+    if review.save
+      redirect_to video_path(video)
+    else
+      # it 'sets @video'
+      # to ensure @video is still there after rendering, and also to eliminate the phatom review record issue
+      @video = video.reload
+      # BUG: flash message still there after navigate to another page
+      flash[:danger] = 'Rating or content cannot be blank'
+      # TODO: not lose review content when rating not present when submitting
+      render 'videos/show'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
---
+++ spec/controllers/reviews_controller_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe ReviewsController do
+
+  describe 'POST create' do
+
+    let!(:seven) { seven = Fabricate(:video) }
+    let!(:bob) { bob = Fabricate(:user) }
+
+    context 'with authenticated user' do
+      context 'with valid input' do
+        before do
+          session[:user_id] = bob.id
+          post :create, review: Fabricate.attributes_for(:review), video_id: seven.id
+        end
+        it 'redirects user to video show page' do
+          expect(response).to redirect_to video_path(seven)
+        end
+        it 'creates review' do
+          expect(Review.count).to eq(1)
+        end
+        it 'creates review associated with the video' do
+          expect(Review.first.video).to eq(seven)
+        end
+        it 'creates review associated with the user' do
+          expect(Review.first.user).to eq(bob)
+        end
+      end
+      context 'with invalid input' do
+        before do
+          session[:user_id] = bob.id
+        end
+        it 'sets @video' do
+          post :create, review: {rating: '', content: ''}, video_id: seven.id
+          expect(assigns(:video)).to eq(seven)
+        end
+        it 'does not create review if no rating' do
+          post :create, review: {rating: '', content: 'something'}, video_id: seven.id
+          expect(Review.count).to eq(0)
+        end
+        it 'does not create review if no content' do
+          post :create, review: {rating: '5', content: ''}, video_id: seven.id
+          expect(Review.count).to eq(0)
+        end
+        it 'renders user to video show template' do
+          post :create, review: {rating: '', content: ''}, video_id: seven.id
+          expect(response).to render_template('videos/show')
+        end
+        it 'sets error message' do
+          post :create, review: {rating: '', content: ''}, video_id: seven.id
+          expect(flash[:danger]).to eq('Rating or content cannot be blank')
+        end
+      end
+    end
+
+    context 'with unauthenticated user' do
+      before do
+        post :create, review: Fabricate.attributes_for(:review), video_id: seven.id
+      end
+      it 'does not create review' do
+        expect(Review.count).to eq(0)
+      end
+      it 'redirects user to sign in page' do
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+
+  end
+
+end
