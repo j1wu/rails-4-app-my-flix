95a3cb38a8e412eddecfd71213ddc7556fc4e64f
j1wu <ji.wu@outlook.com>
2014-07-30 10:13:48 +0800

    refactored feature test followed the solution video

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -15,7 +15,7 @@ feature 'User interacts with queue' do
     expect(page).to have_content('Welcome, ')
 
     # visit video show page
-    find("a[href='/videos/#{annie_hall.id}']").click
+    go_to_video(annie_hall)
     expect(page).to have_content(annie_hall.title)
 
     # add video to queue
@@ -24,49 +24,55 @@ feature 'User interacts with queue' do
 
     # '+My Queue' button shouldn't be there if video exsits in queue
     visit video_path(annie_hall)
-    expect(page).to_not have_content('+ My Queue')
+    expect_link_not_to_be_seen('+ My Queue')
 
     # add more videos to queue
-    visit home_path
-    find("a[href='/videos/#{god_father.id}']").click
-    click_link "+ My Queue"
-    visit home_path
-    # find by anchor sytax: find("a[]")
-    find("a[href='/videos/#{seven.id}']").click
-    click_link "+ My Queue"
-    expect(page).to have_content(annie_hall.title)
-    expect(page).to have_content(god_father.title)
-    expect(page).to have_content(seven.title)
+    add_video_to_queue(god_father)
+    add_video_to_queue(seven)
+    expect_video_to_be_in_queue(annie_hall)
+    expect_video_to_be_in_queue(god_father)
+    expect_video_to_be_in_queue(seven)
 
-    # fill in queue item position, solution #1: changing the html element id 
-    # what it looks like in view: id: "video_#{video.id}"
-    visit my_queue_path
-    fill_in "video_#{annie_hall.id}", with: 3
-    fill_in "video_#{seven.id}", with: 2
-    fill_in "video_#{god_father.id}", with: 1
-    click_button("Update Instant Queue")
-    # find by ID sytax: find("#id")
-    expect(find("#video_#{annie_hall.id}").value).to eq("3")
-    expect(find("#video_#{seven.id}").value).to eq("2")
-    expect(find("#video_#{god_father.id}").value).to eq("1")
+    # fill in queue item position
+    set_video_position(annie_hall, 3)
+    set_video_position(seven, 2)
+    set_video_position(god_father, 1)
 
-    # fill in queue item position, solution #2: adding data attribute
-    # what it looks like in view: data: { video_id: video.id }
-    find("input[data-video-id='#{annie_hall.id}']").set(1)
-    find("input[data-video-id='#{god_father.id}']").set(2)
-    find("input[data-video-id='#{seven.id}']").set(3)
     click_button("Update Instant Queue")
-    expect(find("input[data-video-id='#{annie_hall.id}']").value).to eq("1")
-    expect(find("input[data-video-id='#{god_father.id}']").value).to eq("2")
-    expect(find("input[data-video-id='#{seven.id}']").value).to eq("3")
-    
-    # fill in queue item position, solution #3: scoping feature and xpath
-    within(:xpath, "//tr[contains(.,'#{annie_hall.title}')]") do
-      fill_in "queue_items[][position]", with: 4
-    end
-    click_button("Update Instant Queue")
-    expect(find(:xpath, "//tr[contains(.,'#{annie_hall.title}')]//input[@type='text']").value).to eq("3")
+
+    expect_video_position(annie_hall, 3)
+    expect_video_position(seven, 2)
+    expect_video_position(god_father, 1)
 
   end
+
+    def expect_link_not_to_be_seen(link_text)
+      expect(page).to_not have_content('link_text')
+    end
+
+    def expect_video_to_be_in_queue(video)
+      expect(page).to have_content(video.title)
+    end
+    
+    def go_to_video(video)
+      visit home_path
+      find("a[href='/videos/#{video.id}']").click
+    end
+
+    def add_video_to_queue(video)
+      visit home_path
+      find("a[href='/videos/#{video.id}']").click
+      click_link "+ My Queue"
+    end
+
+    def set_video_position(video, position)
+      within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+        fill_in "queue_items[][position]", with: position
+      end
+    end
+
+    def expect_video_position(video, position)
+      expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
+    end
 
 end
