167087fd67d2bfbeb4756327fda83bb7b99c0e1b
j1wu <ji.wu@outlook.com>
2014-08-03 00:56:53 +0800

    minor fixes per Alberts comments

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,7 +4,8 @@ class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: 'User' 
 
   def duplicated?
-    Relationship.all.map(&:follower).include? self.follower and Relationship.all.map(&:leader).include? self.leader
+    # Relationship.all.map(&:follower).include? self.follower and Relationship.all.map(&:leader).include? self.leader
+    Relationship.pluck(:follower_id).include? self.follower.id and Relationship.pluck(:leader_id).include? self.leader.id
   end
 
 end
