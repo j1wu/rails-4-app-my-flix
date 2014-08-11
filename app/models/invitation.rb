2c63fa4c86ecd36575010ec826e3804d1600103b
j1wu <ji.wu@outlook.com>
2014-08-11 15:02:36 +0800

    moved token generation logic from invitation model into concern

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,11 +1,6 @@
 class Invitation < ActiveRecord::Base
+  include Tokenable
 
   validates_presence_of :invitee_name, :invitee_email, :message
 
-  before_create :generate_token
-
-  private
-  def generate_token
-   self.token = SecureRandom.urlsafe_base64 
-  end
 end
